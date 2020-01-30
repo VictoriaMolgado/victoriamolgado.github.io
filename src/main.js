93fc8e659b82a8042e9530bb940d9872c8583f13
Victória Molgado <victoria.molgado@yahoo.com.br>
2020-01-29 22:21:45 -0300

    Animating key-steps and updating About text 😴

--- src/main.js
+++ src/main.js
@@ -23,3 +23,18 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+var coll = document.getElementsByClassName("key-step");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
